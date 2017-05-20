a37f03fc67a2469fa803818cd1774de6891f81da
khoa.hong <khoa.hong@lazada.com>
2017-05-20 17:49:20 +0700

    [master] Add CucumberTestRunner to run test with gradle

---
+++ src/test/java/com/ebay/CucumberTestRunner.java
@@ -0,0 +1,16 @@
+package com.ebay;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"com.ebay.steps"},
+        format = {"pretty", "html:report/index"},
+        features = "src/test/resources"
+)
+public class CucumberTestRunner {
+
+}
